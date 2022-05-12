88a957d8ab7c03aa20efd6bd4be2bcbb60314c74
shhhaej <hyejin11sa@gmail.com>
2022-05-12 23:24:22 +0900

    Sidenav shape changes according to screen size

---
+++ index.js
@@ -0,0 +1,27 @@
+// const NavBarsBtn = document.querySelector(".NavBarsBtn");
+// const SidenavWrap = document.querySelector(".NavBarsBtn");
+// const Sidenav = document.querySelector(".Sidenav li");
+
+// const SidenavWrapAnimation = () => {
+//   Sidenav.forEach((link, index) => {
+//     //애니메이션이 있을 때
+//     if (link.style.animation) {
+//       //애니메이션 비움
+//       link.style.animation = "";
+//     } else {
+//       //애니메이션 없을 때 애니메이션을 추가
+//       // 딜레이 간격을 줘서 li가 하나씩 차례대로 나타나도록 설정
+//       link.style.animation = `SidenavFade 0.5s ease forwards $ {
+//         index / 7 + 0.5 
+//       }s`;
+//     }
+//   });
+// };
+
+
+// function on() {
+//   document.getElementById("Sidenav_overlay").style.display = "block";
+// }
+// function off() {
+//   document.getElementById("Sidenav_overlay").style.display = "none";
+// }
