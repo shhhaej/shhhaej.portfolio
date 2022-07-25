a860bde42856d9f54a16bd9297e7370b971abc76
shhhaej <hyejin11sa@gmail.com>
2022-07-25 22:32:11 +0900

    Create a scrolling dynamic responsive page using the AOS plugin.

--- index.js
+++ index.js
@@ -7,72 +7,101 @@ function closeNav() {
   document.getElementById("burgerNav").style.height = "0%";
 }
 
-//Screen change
-function slidePage2(direction) {
 
-  if(index == 0) {
-    if(direction == 'left') {
+//로딩중 화면표시
+function loadingProcess() {
+  openLoading('./img/hj.gif');
 
-      index = 2;
+  //타이머를 이용해 로딩창 종료
+  setTimeout(closeLoading, 3000);
 
-      $("#content-two").hide(0);
-      $("#content-one").hide(0);
-      $("content-three").fadeln(0);
-
-    } else if (direction == 'right') {
-
-      index = 1;
-      
-      $("#content-one").hide(0);
-      $("#content-three").hide(0);
-      $("#content-two").fadeln(0);
-
-    }
-  }
 }
 
-function slidePage2(direction) {
+//로딩창 키는 함수
+function openLoading(gif) {
 
-  if(index == 1) {
-    if(direction == 'left') {
+  //화면 높이와 너비를 구함
+  var maskHeight = $(document).height();
+  var maskWidth = window.document.body.clientWidth;
 
-      index = 2;
+  //출력할 마스크를 설정함
+  var mask = "<div id='mask' style='position:absolute; z-index:9000; background-color:#000000; display:none; left:0; top:0;'></div>";
+  
+  //로딩 이미지 주소 및 옵션
+  var loadingImg = '';
+  loadingImg += "<div id='loadingImg' style='position:absolute; left:37%; top:30%; display:none; z-index:10000;'>";
+  loadingImg += "<img src='./img/hj.gif'/>";
+  loadingImg += "</div>";
 
-      $("#content-two").hide(0);
-      $("#content-one").hide(0);
-      $("content-three").fadeln(0);
+  //레이어 추가
+  $('body')
+      .append(mask)
+      .append(loadingImg);
 
-    } else if (direction == 'right') {
+  //마스크의 높이와 너비로 전체 화면을 채움
+  $('#mask').css({
+      'width' : maskWidth
+      , 'height': maskHeight
+      // , 'opacity' : '0.9'
+      , 'background' : '#ff9933'
+  });
 
-      index = 1;
-      
-      $("#content-one").hide(0);
-      $("#content-three").hide(0);
-      $("#content-two").fadeln(0);
+   //마스크 표시
+  $('#mask').show();
 
-    }
-  }
+  //로딩 이미지 표시
+  $('#loadingImg').show();
 }
 
-function slidePage2(direction) {
-
-  if(index == 2) {
-    if(direction == 'left') {
-
-      index = 2;
-
-      $("#content-two").hide(0);
-      $("#content-one").hide(0);
-      $("content-three").fadeln(0);
-
-    } else if (direction == 'right') {
-
-      index = 1;
-      
-      $("#content-one").hide(0);
-      $("#content-three").hide(0);
-      $("#content-two").fadeln(0);
-
-    }
-  }
+  //로딩창 끄는 함수
+  function closeLoading() {
+  $('#mask, #loadingImg').hide();
+  $('#mask, #loadingImg').empty();
 }
+
+
+//스크롤 이동
+$(function(){
+    $("a").on("click", function(){
+      var divName = $(this).attr("id"),
+          topPosition = $("." + divName).offset().top;
+      $('html, body').animate({
+          scrollTop: topPosition - 55
+      }, 700);
+      return false; //리턴펄스로 스크롤이 최상위로 갔다가 돌아오는 현상 없어짐
+    });
+});
+
+
+
+// slick 
+$(function() {
+  $('.sliderWrap').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    infinite: false, //양방향 무한 모션
+    arrows: true,
+    dots: true,
+    dotsClass: "slick-dots", //아래 나오는 페이지네이션 css class 지정
+    vertical: false,
+    prevArrow: "<button type= 'button' class='slick-prev'>prev</button>",
+    nextArrow: "<button type= 'button' class='slick-next'></button>",
+    draggable: true, 
+    responsive: [ //반응형 웹 구현 옵션
+      {
+        breakpoint: 768, //768px 이하
+        settings: { //위에 옵션이 디폴트, 여기에 추가하면 그걸로 변경
+          slidesToShow: 3, //슬라이드 3개 보이고
+          arrows: false, //좌우 버튼 보이지 않기
+        }
+      },
+      {
+        breakpoint: 600, //600px 이하
+        settings: {
+          slidesToShow: 1, //슬라이드 1개 보이고
+          arrows: true, //좌우 버튼 보이기
+        }
+      }
+    ]
+  });
+});
